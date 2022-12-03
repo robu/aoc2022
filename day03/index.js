bec7253f802904457722d6ab615bd7a13cfabae3
Robert Buren <robert@buren.se>
2022-12-03 13:02:40 +0100

    day03 refactor

--- day03/index.js
+++ day03/index.js
@@ -1,11 +1,9 @@
 const fs = require('fs')
 
-const aCode = 'a'.charCodeAt(0)
-const ACode = 'A'.charCodeAt(0)
-
 const input = fs.readFileSync('input.txt', { encoding: 'utf8' }).trim().split('\n')
 
 const splitLines = (lines) => lines.map(line => [line.substring(0, line.length / 2), line.substring(line.length / 2)])
+
 const groupLines = (lines, groupSize = 3) => {
     let newArr = []
     let group = []
@@ -19,29 +17,16 @@ const groupLines = (lines, groupSize = 3) => {
     return newArr
 }
 
-const findMatch = (str1, str2) => {
-    for (let c in str1) {
-        if (str2.includes(str1[c])) {
-            return str1[c]
-        }
-    }
-}
+const matchInAll = (str, arr) => arr.reduce((result, s) => result && s.includes(str), true)
 
-const findMatch3 = (str1, str2, str3) => {
-    for (let c in str1) {
-        if (str2.includes(str1[c]) && str3.includes(str1[c])) {
-            return str1[c]
-        }
-    }
-}
+const findMatchInAll = (str, strArr) => str.split('').find(c => (matchInAll(c, strArr)))
 
-const matches = (arr) => arr.map(e => findMatch(e[0], e[1]))
-
+const aCode = 'a'.charCodeAt(0)
+const ACode = 'A'.charCodeAt(0)
 const prio = (char) => char.match(/[a-z]/) ? char.charCodeAt(0) - aCode + 1 : char.charCodeAt(0) - ACode + 27
 
-const part1 = () => matches(splitLines(input)).map(x => prio(x)).reduce((s, p) => s + p, 0)
+const part1 = () => splitLines(input).map(e => findMatchInAll(e[0], [e[1]])).map(x => prio(x)).reduce((s, p) => s + p, 0)
 
-const part2 = () => groupLines(input).map(grp => findMatch3(grp[0], grp[1], grp[2])).map(x => prio(x)).reduce((s, p) => s + p, 0)
-
+const part2 = () => groupLines(input).map(grp => findMatchInAll(grp[0], [grp[1], grp[2]])).map(x => prio(x)).reduce((s, p) => s + p, 0)
 
 console.log((process.env.part || "part1") == "part1" ? part1() : part2())
