2bd3c58b2c7ec476eac071f6c8e728cc8c0507a7
Robert Buren <robert@buren.se>
2022-12-04 10:57:06 +0100

    day01 small refactor

--- day01/index.js
+++ day01/index.js
@@ -3,11 +3,6 @@ const { InputData } = require('aoc-toolbox')
 const input = new InputData().sections().map(id => id.linesInts().reduce((sum, a) => sum + a, 0))
 
 const part1 = () => input.reduce((high, a) => high > a ? high : a, -1) // highest calorie value
-
-const part2 = () => {
-    input.sort((x, y) => y - x)
-    return input[0] + input[1] + input[2]
-}
+const part2 = () => input.sort((x, y) => y - x).slice(0, 3).reduce((s, e) => s + e, 0)
 
 console.log((process.env.part || "part1") == "part1" ? part1() : part2())
-
