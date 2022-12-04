9780acd044de1d02462a55f55b845184fc084c9e
Robert Buren <robert@buren.se>
2022-12-04 10:10:36 +0100

    day04 done. also added Dockerfile.

--- day04/index.js
+++ day04/index.js
@@ -5,6 +5,9 @@ const input = fs.readFileSync('input.txt', { encoding: 'utf8' }).trim().split('\
 const isContainedBy = (x1, x2, y1, y2) => x1 >= y1 && x2 <= y2
 const containsOrIsContainedBy = (x1, x2, y1, y2) => isContainedBy(x1, x2, y1, y2) || isContainedBy(y1, y2, x1, x2)
 
-const part1 = () => input.map(line=>containsOrIsContainedBy(line[0][0],line[0][1],line[1][0],line[1][1])).filter(v=>v).length
+const hasOverlap = (x1, x2, y1, y2) => isContainedBy(x1, x1, y1, y2) || isContainedBy(x2, x2, y1, y2) || containsOrIsContainedBy(x1, x2, y1, y2)
+
+const part1 = () => input.filter(line => containsOrIsContainedBy(line[0][0], line[0][1], line[1][0], line[1][1])).length
+const part2 = () => input.filter(line => hasOverlap(line[0][0], line[0][1], line[1][0], line[1][1])).length
 
 console.log((process.env.part || "part1") == "part1" ? part1() : part2())
