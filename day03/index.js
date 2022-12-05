7ec7d339c787cc9424a28d303c2da994226dfb7b
Robert Buren <robert@buren.se>
2022-12-06 00:13:03 +0100

    day03 small refactor

--- day03/index.js
+++ day03/index.js
@@ -9,9 +9,7 @@ const groupLines = (arr, size = 3) => [...Array(Math.ceil(arr.length / size))].m
 
 const findMatchInAll = (str, strArr) => str.split('').find(c => strArr.reduce((result, s) => result && s.includes(c), true))
 
-const aCode = 'a'.charCodeAt(0)
-const ACode = 'A'.charCodeAt(0)
-const prio = (char) => char.match(/[a-z]/) ? char.charCodeAt(0) - aCode + 1 : char.charCodeAt(0) - ACode + 27
+const prio = (char) => char.match(/[a-z]/) ? char.charCodeAt(0) - 'a'.charCodeAt(0) + 1 : char.charCodeAt(0) - 'A'.charCodeAt(0) + 27
 
 const part1 = () => splitLines(input).map(e => findMatchInAll(e[0], [e[1]])).reduce((s, c) => s + prio(c), 0)
 
