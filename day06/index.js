8190e3e49e3b7393d3c62e34156d55de06531dda
Robert Buren <robert@buren.se>
2022-12-06 13:35:11 +0100

    day06 refactor

--- day06/index.js
+++ day06/index.js
@@ -2,17 +2,7 @@ const fs = require('fs')
 
 const input = fs.readFileSync('input.txt', { encoding: 'utf8' }).trim().split('')
 
-const hasDuplicate = (arr) => {
-    if (arr.length <= 1) {
-        return false
-    } else if (arr.slice(1).indexOf(arr[0]) >= 0) {
-        return true
-    } else {
-        return hasDuplicate(arr.slice(1)) // recursion FTW!
-    }
-}
-
-const findUniqueSequence = (data, length) => length + data.findIndex((c, i) => !hasDuplicate(data.slice(i, i + length)))
+const findUniqueSequence = (data, length) => length + data.findIndex((c, i) => new Set(data.slice(i, i + length)).size == length)
 
 const part1 = () => findUniqueSequence(input, 4)
 const part2 = () => findUniqueSequence(input, 14)
