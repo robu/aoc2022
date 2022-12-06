de1b137ef306b9f4ea6e532cc552523d9f70235a
Robert Buren <robert@buren.se>
2022-12-06 13:44:01 +0100

    day06 removed unused variable

--- day06/index.js
+++ day06/index.js
@@ -2,6 +2,6 @@ const fs = require('fs')
 
 const input = fs.readFileSync('input.txt', { encoding: 'utf8' }).trim().split('')
 
-const findUniqueSequence = (data, length) => length + data.findIndex((c, i) => new Set(data.slice(i, i + length)).size == length)
+const findUniqueSequence = (data, length) => length + data.findIndex((_, i) => new Set(data.slice(i, i + length)).size == length)
 
 console.log((process.env.part || "part1") == "part1" ? findUniqueSequence(input, 4) : findUniqueSequence(input, 14))
