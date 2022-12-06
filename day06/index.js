cbaffdaac865b4e54d645d34d886e9155c18b99d
Robert Buren <robert@buren.se>
2022-12-06 13:36:11 +0100

    day06 small refactor

--- day06/index.js
+++ day06/index.js
@@ -4,7 +4,4 @@ const input = fs.readFileSync('input.txt', { encoding: 'utf8' }).trim().split(''
 
 const findUniqueSequence = (data, length) => length + data.findIndex((c, i) => new Set(data.slice(i, i + length)).size == length)
 
-const part1 = () => findUniqueSequence(input, 4)
-const part2 = () => findUniqueSequence(input, 14)
-
-console.log((process.env.part || "part1") == "part1" ? part1() : part2())
+console.log((process.env.part || "part1") == "part1" ? findUniqueSequence(input, 4) : findUniqueSequence(input, 14))
