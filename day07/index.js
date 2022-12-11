97e7c44cf671b9aaced833f9e340f4f1a10b7b71
Robert Buren <robert@buren.se>
2022-12-11 17:00:21 +0100

    day07 complete rewrite - now working!

--- day07/index.js
+++ day07/index.js
@@ -1,147 +1,95 @@
 const fs = require('fs')
 
-const input = fs.readFileSync('input.txt', { encoding: 'utf8' }).trim().split('\n')
+const input = fs.readFileSync('input.txt', 'utf8').trim().split('\n')
 
-const cl = l => console.log(l)
-const jl = j => cl(JSON.stringify(j, null, 2))
-
-const createDirInTree = (tree, pathArr) => {
-    //cl(`createDirInTree "${pathArr}"`)
-    if (pathArr.length == 1 && pathArr[0] == tree.name) {
-        return tree
-    } else if (pathArr.length > 1 && pathArr[0] == tree.name) {
-        return createDirInTree(tree, pathArr.slice(1))
-    } else {
-        //cl(`looking for node "${pathArr[0]}"`)
-        let node = tree.entries.find(e => e.name === pathArr[0])
-        if (node) {
-            if (pathArr.length == 1) {
-                return node
-            } else {
-                return createDirInTree(node, pathArr.slice(1))
-            }
-        } else {
-            // create node here
-            tree.entries.push({
-                name: pathArr[0],
-                type: 'dir',
-                size: -1,
-                entries: []
-            })
-            return createDirInTree(tree, pathArr)
-        }
-    }
-}
-
-const doCdCommand = (arg, currentDir) => {
-    let path = currentDir
-    if (arg === '..') {
+const doCdCommand = (arg, path) => {
+    if (arg === '/') {
+        return []
+    } else if (arg === '..') {
         path.pop()
-    } else if (arg === '/') {
-        path = ['/'] // root dir
     } else {
         path.push(arg)
     }
     return path
 }
 
-const doCommand = (line, currentDir) => {
-    let matches = line.match(/^\$ cd (.+)$/)
-    if (matches) {
-        return doCdCommand(matches[1], currentDir)
-    } else if (line.match(/^\$ ls/)) {
-        return currentDir
+const findOrCreateNode = (tree, path) => {
+    let node = tree
+    if (path.length > 0) {
+        let subnode = node.dirEntries[path[0]]
+        if (!subnode) {
+            subnode = node.dirEntries[path[0]] = { dirEntries: {}, fileEntries: {} }
+        }
+        node = subnode
+        return findOrCreateNode(node, path.slice(1))
     } else {
-        return false
+        return node
     }
 }
 
-const addFileToDir = (tree, path, fileLine) => {
-    let matches = fileLine.match(/^(\d+) (.+)$/)
-    if (matches) {
-        let fileSize = parseInt(matches[1])
-        let fileName = matches[2]
-        let node = createDirInTree(tree, path)
-        node.entries.push({
-            name: fileName,
-            type: 'file',
-            size: fileSize
-        })
-    } else {
-        // cl(`ignoring line "${fileLine}"`)
-    }
-}
-
-const calcSizes2 = (treeNode) => {
-    if (treeNode.type === 'dir') {
-        treeNode.size = treeNode.entries.reduce((s, e) => s + calcSizes2(e), 0)
-    }
-    return treeNode.size
-}
-
-// const calcSizes = (treeNode) => {
-//     let size = 0
-//     treeNode.entries.forEach((entry) => {
-//         if (entry.type == 'file') {
-//             size += entry.size
-//         } else if (entry.type == 'dir') {
-//             size += calcSizes(entry)
-//         } else {
-//             throw `unknown entry type: ${entry.type}`
-//         }
-//     })
-//     treeNode.size = size
-//     return size
-// }
-
-const buildTree = (lines) => {
-    let dirTree = {
-        name: '/',
-        type: 'dir',
-        size: -1,
-        entries: []
-    }
-    let currentDir = []
-    lines.forEach((line) => {
-        let newDir = doCommand(line, currentDir)
-        if (newDir) {
-            currentDir = newDir
-            createDirInTree(dirTree, currentDir)
+const readData = (lines) => {
+    let path = []
+    let tree = { fileEntries: {}, dirEntries: {} } // contains name, fileEntries and dirEntries. Each dirEntry does the same.
+    let currentNode = tree
+    lines.forEach(line => {
+        let matches = null
+        if (matches = line.match(/\$ cd (.+)$/)) {
+            path = doCdCommand(matches[1], path)
+            currentNode = findOrCreateNode(tree, path)
+        } else if (matches = line.match(/\$ ls/)) {
+            // ignore ls commands
+        } else if (matches = line.match(/dir .+/)) {
+            // ignore dir listings
+        } else if (matches = line.match(/(\d+) (.+)$/)) {
+            let [_, sizeStr, name] = matches
+            let fileSize = parseInt(sizeStr)
+            currentNode.fileEntries[name] = fileSize
         } else {
-            addFileToDir(dirTree, currentDir, line)
+            throw `Unknown line format, can't parse: "${line}"`
         }
     })
-    calcSizes2(dirTree)
-    return dirTree
+    return tree
 }
 
-const filterDirSizes = (treeNode) => {
-    let arr = []
-    if (treeNode.type === 'dir') {
-        arr.push({ name: treeNode.name, size: treeNode.size })
-        treeNode.entries.forEach(entry => arr.push(...filterDirSizes(entry)))
+const calcDirSize = (tree) => {
+    let fileTotal = Object.values(tree.fileEntries).reduce((s, v) => s + v, 0)
+    let dirTotal = Object.values(tree.dirEntries).reduce((s, dirEntry) => s + calcDirSize(dirEntry), 0)
+    tree.size = fileTotal + dirTotal
+    return tree.size
+}
+
+const filterDirSizes = (tree, maximum, arr = []) => {
+    if (tree.size <= maximum) {
+        arr.push(tree.size)
     }
+    Object.values(tree.dirEntries).forEach(node => filterDirSizes(node, maximum, arr))
     return arr
 }
 
-const filterNodes = (tree, pred) => {
-    let arr = []
-    if (pred(tree)) {
-        arr.push(tree)
+const findSmallestDirAtLeastSize = (tree, threshold, bestSoFar) => {
+    if (tree.size >= threshold && tree.size < bestSoFar) {
+        bestSoFar = tree.size
     }
-    if (tree.type == 'dir') {
-        tree.entries.forEach((e) => arr.push(...filterNodes(e, pred)))
-    }
-    return arr
+    Object.values(tree.dirEntries).forEach(subdir => bestSoFar = findSmallestDirAtLeastSize(subdir, threshold, bestSoFar))
+    return bestSoFar
 }
+
 
 const part1 = () => {
-    let tree = buildTree(input)
-    //jl({ tree })
-    let dirSizes = filterDirSizes(tree).filter(e => e.size <= 100000)
-    //jl({ dirSizes })
+    let tree = readData(input)
+    calcDirSize(tree)
+    let arr = filterDirSizes(tree, 100000)
+    return arr.reduce((s, v) => s + v, 0)
+}
 
-    return dirSizes.reduce((s, v) => s + v.size, 0)
+const part2 = () => {
+    let tree = readData(input)
+    calcDirSize(tree)
+
+    let currentFreeSpace = 70000000 - tree.size
+    let spaceNeeded = 30000000 - currentFreeSpace
+    return findSmallestDirAtLeastSize(tree, spaceNeeded, tree.size+1)
 }
 
 console.log((process.env.part || "part1") == "part1" ? part1() : part2())
+
